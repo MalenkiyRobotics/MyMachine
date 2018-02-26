99a1b2c9e12c4683ac10dc9cd2e2242462e4d041
Type3Lab <gergely06@gmail.com>
2018-02-26 22:07:42 +0100

    init

---
+++ src/main/java/neramon/phazero/mymachine/Register.java
@@ -0,0 +1,40 @@
+package neramon.phazero.mymachine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+
+public class Register {
+
+    public static void main(String[] args) throws IOException {
+        URL url;
+        BufferedReader in;
+        String ipAddress;
+        try {
+            url = new URL("http://bot.whatismyipaddress.com");
+            in = new BufferedReader(new InputStreamReader(url.openStream()));
+            ipAddress = in.readLine().trim();
+            if (!(ipAddress.length() > 0)) {
+                try {
+                    InetAddress ip = InetAddress.getLocalHost();
+                    System.out.println((ip.getHostAddress()).trim());
+                    ipAddress = (ip.getHostAddress()).trim();
+                } catch(Exception exp) {
+                    ipAddress = "ERROR";
+                }
+            }
+        } catch (Exception ex) {
+            try {
+                InetAddress ip = InetAddress.getLocalHost();
+                System.out.println((ip.getHostAddress()).trim());
+                ipAddress = (ip.getHostAddress()).trim();
+            } catch(Exception exp) {
+                ipAddress = "ERROR";
+            }
+        }
+        System.out.println("IP Address: " + ipAddress);
+    }
+
+}
