7b90ce2086208b64f88d89435e137238d85aaff0
ekovger <ekovger@localhost.localdomain>
2018-02-27 11:47:53 +0100

    5000

--- src/main/java/neramon/phazero/mymachine/SimpleSrv.java
+++ src/main/java/neramon/phazero/mymachine/SimpleSrv.java
@@ -11,7 +11,7 @@ import com.sun.net.httpserver.HttpServer;
 public class SimpleSrv {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(5000), 0);
         server.createContext("/testme", new MyHandler());
         server.setExecutor(null);
         server.start();
@@ -29,4 +29,3 @@ public class SimpleSrv {
     }
 
 }
-
